efa5491d17a3202d51b91f1bb343c47458acf5b6
Ben1138 <marten.mildt@web.de>
2021-01-19 14:43:35 +0100

    Put everything into LightNet namespace. Added documentation. Removed Example.

--- LogQueue.cs
+++ LogQueue.cs
@@ -1,50 +1,59 @@
 ﻿using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 
-public enum ELogType
+namespace LightNet
 {
-    Info,
-    Warning,
-    Error
-}
-
-public struct LogMessage
-{
-    public ELogType Type;
-    public string Message;
-}
-
-public static class LogQueue
-{
-    static ConcurrentQueue<LogMessage> Logs = new ConcurrentQueue<LogMessage>();
-
-    static void Log(ELogType type, string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+    public enum ELogType
     {
-        caller = System.IO.Path.GetFileName(caller);
-        args = args == null ? new object[0] : args;
-        LogMessage msg;
-        msg.Type = type;
-        msg.Message = string.Format("[{0}:{1}]", caller, lineNumber) + string.Format(format, args);
-        Logs.Enqueue(msg);
+        Info,
+        Warning,
+        Error
     }
 
-    public static bool GetNext(out LogMessage nextLogMessage)
+    public struct LogMessage
     {
-        return Logs.TryDequeue(out nextLogMessage);
+        public ELogType Type;
+        public string Message;
     }
 
-    public static void LogInfo(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+    /// <summary>
+    /// A thread safe log message queue, usefull to share log messages between different threads. You can queue and unqueue log messages from anywhere in any thread.<br/>
+    /// All log methods provide additional auto-filled parameters to determine the caller method name and line number.<br/>
+    /// Example:
+    /// <code>LogQueue.LogInfo("This is a test.");</code>
+    /// </summary>
+    public static class LogQueue
     {
-        Log(ELogType.Info, format, args, caller, lineNumber);
-    }
+        static ConcurrentQueue<LogMessage> Logs = new ConcurrentQueue<LogMessage>();
 
-    public static void LogWarning(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
-    {
-        Log(ELogType.Warning, format, args, caller, lineNumber);
-    }
+        static void Log(ELogType type, string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+        {
+            caller = System.IO.Path.GetFileName(caller);
+            args = args == null ? new object[0] : args;
+            LogMessage msg;
+            msg.Type = type;
+            msg.Message = string.Format("[{0}:{1}]", caller, lineNumber) + string.Format(format, args);
+            Logs.Enqueue(msg);
+        }
 
-    public static void LogError(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
-    {
-        Log(ELogType.Error, format, args, caller, lineNumber);
+        public static bool GetNext(out LogMessage nextLogMessage)
+        {
+            return Logs.TryDequeue(out nextLogMessage);
+        }
+
+        public static void LogInfo(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+        {
+            Log(ELogType.Info, format, args, caller, lineNumber);
+        }
+
+        public static void LogWarning(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+        {
+            Log(ELogType.Warning, format, args, caller, lineNumber);
+        }
+
+        public static void LogError(string format, object[] args = null, [CallerFilePathAttribute] string caller = null, [CallerLineNumber] int lineNumber = 0)
+        {
+            Log(ELogType.Error, format, args, caller, lineNumber);
+        }
     }
 }
